12b65ee3342da716d78d1f5875b66c05d35a6e9f
Andres Picazo <apycazo@gmail.com>
2017-01-09 22:47:50 +0100

    Created new sample path for spring-data-basic

---
+++ spring-data-basic/src/main/java/com/github/apycazo/codex/spring/data/basic/Record.java
@@ -0,0 +1,36 @@
+package com.github.apycazo.codex.spring.data.basic;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Builder
+@Entity
+@Table(name = "records")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Record
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    private String name;
+    private Integer updates;
+
+    @PrePersist
+    protected void beforeCreate ()
+    {
+        updates = 0;
+    }
+
+    @PreUpdate
+    protected void beforeUpdate ()
+    {
+        updates++;
+    }
+
+}
---
+++ spring-data-basic/src/main/java/com/github/apycazo/codex/spring/data/basic/RecordRepository.java
@@ -0,0 +1,8 @@
+package com.github.apycazo.codex.spring.data.basic;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface RecordRepository extends CrudRepository<Record, Long>
+{
+    // Empty
+}
