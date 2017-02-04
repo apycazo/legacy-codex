cc7c228a3210892ee17f417951f68e48ec77832c
Andres Picazo <apycazo@gmail.com>
2017-02-04 10:45:19 +0100

    Moustache ref improved to include bootstrap styles Feigned client added headers

--- codex-spring-rest/src/main/java/com/github/apycazo/codex/spring/rest/feign/FeignedServiceAPI.java
+++ codex-spring-rest/src/main/java/com/github/apycazo/codex/spring/rest/feign/FeignedServiceAPI.java
@@ -1,6 +1,10 @@
 package com.github.apycazo.codex.spring.rest.feign;
 
-import org.springframework.web.bind.annotation.*;
+import feign.Headers;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * A couple notes:
@@ -12,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 public interface FeignedServiceAPI
 {
     @RequestMapping(value = "feign/echo/{text}", method = RequestMethod.GET)
+    @Headers("Content-Type: application/json") // only to show how to send headers
     String echo (@PathVariable(value = "text") String text);
     @RequestMapping(value = "feign/reverse", method = RequestMethod.POST)
     String reverse(@RequestBody String text);
