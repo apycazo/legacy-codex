c38541d476c11a246f58403bfbd9be950a5861a1
Andres Picazo <apycazo@gmail.com>
2017-02-04 11:48:10 +0100

    Removed codex-spring-data

--- codex-spring-data/src/main/java/com/github/apycazo/codex/spring/data/basic/RecordRepository.java
+++
@@ -1,8 +0,0 @@
-package com.github.apycazo.codex.spring.data.basic;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface RecordRepository extends CrudRepository<Record, Long>
-{
-    // Empty
-}
--- codex-spring-data/src/test/java/com/github/apycazo/codex/spring/data/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Created by manager on 10/01/17.
- */
-package com.github.apycazo.codex.spring.data;
