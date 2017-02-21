f5819d6cfadbcd335bfa25dd247eec826b773f4e
Andres Picazo <apycazo@gmail.com>
2017-02-21 20:12:02 +0100

    Added more tests for mocked servers

--- codex-spring/codex-spring-rest/src/test/java/com/github/apycazo/codex/spring/rest/mock/MockedRestServiceTest.java
+++ codex-spring/codex-spring-rest/src/test/java/com/github/apycazo/codex/spring/rest/mock/MockedRestServiceTest.java
@@ -18,6 +18,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.springframework.test.web.client.ExpectedCount.manyTimes;
+import static org.springframework.test.web.client.ExpectedCount.min;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
@@ -48,5 +49,120 @@ public class MockedRestServiceTest
         assertNotNull(info);
         assertEquals(1, info.getId());
         assertEquals("test", info.getUsername());
+    }
+
+    @Test(expected = AssertionError.class)
+    public void makeCallsInOrderAndValidatedExpectingError ()
+    {
+        MockRestServiceServer server = MockRestServiceServer.bindTo(restTemplate).build();
+        // first query expected (note that 'manyTimes()' means 1+)
+        String firstQueryUrl = "http://127.0.0.1:9000/one";
+        String firstQueryResponse = "{\"key\":1}";
+        server
+                .expect(manyTimes(), requestTo(firstQueryUrl))
+                .andExpect(method(HttpMethod.GET))
+                .andRespond(withSuccess(firstQueryResponse, MediaType.APPLICATION_JSON));
+        // second query expected
+        String secondQueryUrl = "http://127.0.0.1:9000/two";
+        String secondQueryResponse = "{\"key\":2}";
+        server
+                .expect(min(2), requestTo(secondQueryUrl))
+                .andExpect(method(HttpMethod.GET))
+                .andRespond(withSuccess(secondQueryResponse, MediaType.APPLICATION_JSON));
+        // now make the calls and verify
+        String response;
+        response = restTemplate.getForObject(firstQueryUrl, String.class);
+        assertEquals(firstQueryResponse, response);
+        response = restTemplate.getForObject(secondQueryUrl, String.class);
+        assertEquals(secondQueryResponse, response);
+        // I expect this to throw an exception
+        server.verify();
+    }
+
+    @Test
+    public void makeCallsInOrderAndValidated ()
+    {
+        MockRestServiceServer server = MockRestServiceServer.bindTo(restTemplate).build();
+        // first query expected (note that 'manyTimes()' means 1+)
+        String firstQueryUrl = "http://127.0.0.1:9000/one";
+        String firstQueryResponse = "{\"key\":1}";
+        server
+                .expect(manyTimes(), requestTo(firstQueryUrl))
+                .andExpect(method(HttpMethod.GET))
+                .andRespond(withSuccess(firstQueryResponse, MediaType.APPLICATION_JSON));
+        // second query expected
+        String secondQueryUrl = "http://127.0.0.1:9000/two";
+        String secondQueryResponse = "{\"key\":2}";
+        server
+                .expect(min(2), requestTo(secondQueryUrl))
+                .andExpect(method(HttpMethod.GET))
+                .andRespond(withSuccess(secondQueryResponse, MediaType.APPLICATION_JSON));
+        // now make the calls and verify
+        String response;
+        response = restTemplate.getForObject(firstQueryUrl, String.class);
+        assertEquals(firstQueryResponse, response);
+        response = restTemplate.getForObject(secondQueryUrl, String.class);
+        assertEquals(secondQueryResponse, response);
+        response = restTemplate.getForObject(secondQueryUrl, String.class);
+        assertEquals(secondQueryResponse, response);
+        // I expect this to succeed
+        server.verify();
+    }
+
+    @Test(expected = AssertionError.class)
+    public void makeCallsNotInOrder ()
+    {
+        MockRestServiceServer server = MockRestServiceServer.bindTo(restTemplate).build();
+        // first query expected (note that 'manyTimes()' means 1+)
+        String firstQueryUrl = "http://127.0.0.1:9000/one";
+        String firstQueryResponse = "{\"key\":1}";
+        server
+                .expect(manyTimes(), requestTo(firstQueryUrl))
+                .andExpect(method(HttpMethod.GET))
+                .andRespond(withSuccess(firstQueryResponse, MediaType.APPLICATION_JSON));
+        // second query expected
+        String secondQueryUrl = "http://127.0.0.1:9000/two";
+        String secondQueryResponse = "{\"key\":2}";
+        server
+                .expect(manyTimes(), requestTo(secondQueryUrl))
+                .andExpect(method(HttpMethod.GET))
+                .andRespond(withSuccess(secondQueryResponse, MediaType.APPLICATION_JSON));
+        // now make the calls and verify
+        String response;
+        // make the second expected call before
+        response = restTemplate.getForObject(secondQueryUrl, String.class);
+        assertEquals(secondQueryResponse, response);
+        response = restTemplate.getForObject(firstQueryUrl, String.class);
+        assertEquals(firstQueryResponse, response);
+    }
+
+    @Test
+    public void makeCallsAllowingAnyOrder ()
+    {
+        // configure mocked server to ignore order
+        MockRestServiceServer server = MockRestServiceServer.bindTo(restTemplate).ignoreExpectOrder(true).build();
+        // first query expected (note that 'manyTimes()' means 1+)
+        String firstQueryUrl = "http://127.0.0.1:9000/one";
+        String firstQueryResponse = "{\"key\":1}";
+        server
+                .expect(manyTimes(), requestTo(firstQueryUrl))
+                .andExpect(method(HttpMethod.GET))
+                .andRespond(withSuccess(firstQueryResponse, MediaType.APPLICATION_JSON));
+        // second query expected
+        String secondQueryUrl = "http://127.0.0.1:9000/two";
+        String secondQueryResponse = "{\"key\":2}";
+        server
+                .expect(manyTimes(), requestTo(secondQueryUrl))
+                .andExpect(method(HttpMethod.GET))
+                .andRespond(withSuccess(secondQueryResponse, MediaType.APPLICATION_JSON));
+        // now make the calls and verify
+        String response;
+        // make the second expected call before
+        response = restTemplate.getForObject(secondQueryUrl, String.class);
+        assertEquals(secondQueryResponse, response);
+        response = restTemplate.getForObject(firstQueryUrl, String.class);
+        assertEquals(firstQueryResponse, response);
+        // verify
+        server.verify();
     }
 }
